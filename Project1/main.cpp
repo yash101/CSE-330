8ba6e91a4753aad81879eab99bff192d12e449ed
yash101 <lodhad@gmail.com>
2019-02-04 22:37:19 -0700

    Uploading changed files

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,9 +1,10 @@
-#include "regex_parser_single.h"
-#include "mmap_file.h"
 #include <vector>
 #include <cstdio>
 #include <regex.h>
 #include <cstring>
+#include <regex>
+#include <fstream>
+#include <iostream>
 
 void help(int argc, char** argv) {
 	printf("%s <pattern> <list of text files>\n", argv[0]);
@@ -22,88 +23,28 @@ public:
 	AutoPointer(T* p, size_t ct) :
 		ptr(p),
 		n(ct)
-	{}
-
-	AutoPointer(size_t ct) :
-
-		ptr(new T[ct]),
-		n(ct)
-	{}
-
-	~AutoPointer() {
-		if (ptr) {
-			if (n == 1)
-				delete ptr;
-			else
-				delete[] ptr;
-		}
+	{
+		printf("Pointer: %p\n", ptr);
 	}
 
-	operator T*() { return ptr; }
+	AutoPointer(size_t ct) {
+		ptr = (T*) calloc(sizeof(T), ct);
+		n = ct;
+		printf("Pointer: %p\n", ptr);
+	}
+
+//	~AutoPointer() {
+//		if (ptr && n != 0) {
+//			printf("Before free Pointer: %p\n", ptr);
+////			free(ptr);
+//			ptr = NULL;
+//			printf("After free Pointer: %p\n", ptr);
+//			n = 0;
+//		}
+//	}
+
+	T* getPtr() const { T* pt; memcpy((void*)&pt, (void*)&ptr, sizeof(T*)); return pt; }
 	size_t& size() { return n; }
-};
-
-class RegexStruct {
-	regex_t r;
-	bool compiled;
-
-public:
-
-	RegexStruct() : compiled(false) {}
-
-	bool compile(std::string regex) {
-		int status = regcomp(&r, regex.c_str(), REG_EXTENDED | REG_NEWLINE);
-		if (status != 0) {
-			char error_msg[0x1 << 3];
-			regerror(status, &r, error_msg, 0x1 << 3);
-			printf("Error compiling regex: %s\n", const_cast<const char*>(error_msg));
-			return false;
-		}
-		return true;
-	}
-
-	bool search(std::string line) {
-		AutoPointer<char> p(line.size());
-		strcpy(p, line.c_str());
-		char* str_p = p;
-		const size_t n_matches = 10;
-		regmatch_t matches[n_matches];
-		
-		while (true) {
-			size_t i = 0;
-			int nomatch = regexec(&r, p, n_matches, matches, 0);
-			if (nomatch) {
-				return false;
-			}
-
-			for (i = 0; i < n_matches; i++) {
-				int start;
-				int finish;
-
-				if (matches[i].rm_so == -1) {
-					break;
-				}
-
-				start = matches[i].rm_so + (str_p - p);
-				finish = matches[i].rm_eo + (str_p - p);
-				if (i == 0) {
-					printf("$& is");
-				}
-				else {
-					printf("$%d is ", i);
-				}
-
-				printf("'%.*s' (bytes %d:%d)\n", (finish - start), p + start, start, finish);
-			}
-			str_p += matches[0].rm_eo;
-		}
-		return 0;
-	}
-
-	~RegexStruct() {
-		if (compiled)
-			regfree(&r);
-	}
 };
 
 bool isLineEnding(const unsigned char* str, size_t pos) {
@@ -114,25 +55,35 @@ bool isLineEnding(const unsigned char* str, size_t pos) {
 	return false;
 }
 
-bool searchFile(RegexStruct& r, std::string fname) {
-	std::vector<std::string> lines;
-	MemoryMappedFile file(fname, MemoryMappedFile::MODE_READ);
-	size_t start = 0;
-	for (size_t i = 0; i < file.getFileSize(); i++) {
-		if (isLineEnding(static_cast<const unsigned char*>(file), i)) {
-			start = i + 1;
-			std::string buf;
-			buf.resize(i - start);
-			for (size_t j = start; j < i; j++) {
-				buf[j] = file[j];
-			}
-			lines.push_back(buf);
-			start = i + 1;
-		}
-	}
+/*
+output = 
+*/
 
-	for (std::vector<std::string>::const_iterator it = lines.begin(); it != lines.end(); ++it) {
-		r.search(*it);
+bool searchFile(std::string regex_str, std::string fname) {
+	std::vector<std::string> lines;
+	std::regex reg(regex_str);
+
+	std::ifstream fin(fname);
+	std::string buffer;
+	while (std::getline(fin, buffer))
+		lines.push_back(buffer);
+
+	for (size_t i = 0; i < lines.size(); i++) {
+		std::smatch match;
+		if (std::regex_search(lines[i], match, std::regex(regex_str))) {
+			std::string pre = match.prefix();
+			std::string m = match[0].str();
+			size_t start_col = pre.size() - 1;
+			size_t end_col = start_col + m.size();
+			const char* str = m.c_str();
+			printf("%s, %zd, %d, %d: %s\n",
+				fname.c_str(),
+				i,
+				start_col,
+				end_col,
+				str
+			);
+		}
 	}
 }
 
@@ -144,11 +95,14 @@ int main(int argc, char** argv)
 	}
 
 	std::string regex_str(argv[1]);
-	RegexStruct reg;
-	reg.compile(regex_str);
-	
+
+	std::vector<std::string> files;
 
 	for (int i = 2; i < argc; i++) {
-		searchFile(reg, std::string(argv[i]));
+		files.push_back(argv[i]);
+	}
+
+	for (std::vector<std::string>::const_iterator it = files.begin(); it != files.end(); ++it) {
+		searchFile(regex_str, *it);
 	}
 }
