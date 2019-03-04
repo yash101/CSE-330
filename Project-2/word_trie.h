555f2c0024a27e1f49f7b853de55b9f0449657c8
yash101 <lodhad@gmail.com>
2019-03-04 13:41:57 -0700

    Working search trie

--- Project-2/word_trie.h
+++ Project-2/word_trie.h
@@ -9,6 +9,7 @@ class WordTrieNode
 public:
 	WordTrieNode* letter[256];
 	std::atomic<size_t> count;
+	char ch;
 
 	WordTrieNode() :
 		letter(),
@@ -23,8 +24,13 @@ private:
 
 public:
 
+	typedef void(*IterateFunc_Cb_t)(std::string& str, size_t count);
+
 	size_t get_count(char* str);
 	void increment_word(char* str);
+	void process_words(char* str);
+
+	void iterate(IterateFunc_Cb_t cb);
 };
 
 #endif
