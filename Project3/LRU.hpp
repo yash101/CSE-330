4608bab22072ad8560ff7fc77b56f4de33674ec1
yash101 <lodhad@gmail.com>
2019-04-05 15:29:09 -0700

    Adding project 3

---
+++ Project3/LRU.hpp
@@ -0,0 +1,19 @@
+#ifndef _LRH_H
+#define _LRH_H
+
+#include <unordered_map>
+
+namespace LRU
+{
+	template <typename Key>
+	class LRU
+	{
+	private:
+	protected:
+	public:
+		LRU();
+		~LRU();
+	};
+}
+
+#endif
